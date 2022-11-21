89e098242c35e591248b5086d7259fbb472e8481
олег <oleg.ru1234567890@mail.ru>
2022-11-21 11:33:06 +0300

    Add function for lab

--- btree/src/btree.erl
+++ btree/src/btree.erl
@@ -1,11 +1,11 @@
 -module(btree).
 
--export([initBT/0, isBT/1, insert_bt/2, isEmpty_bt/1, equal_bt/2, test_all/0, filtration_tree/2, increase_bt/1, add_tree/2, list_to_tree/1, sum_bt/1,mulList/2, multiply_bt/2]).
+-export([init_bt/0, isBT/1, insert_bt/2, isempty_bt/1, equal_bt/2, test_all/0, filtration_tree/2, increase_bt/1, add_tree/2, list_to_tree/1, sum_bt/1, mul_list/2, multiply_bt/2]).
 
 
-initBT() -> {}.
+init_bt() -> {}.
 
-isBT(B) -> {_LMin, _RMax, _H, T} = reku(B), (T or isEmpty_bt(B)).
+isBT(B) -> {_LMin, _RMax, _H, T} = reku(B), (T or isempty_bt(B)).
 
 % Лист
 reku({W, {}, {}, H}) -> {W, W, H, is_number(W) and is_number(H) and (H == 1)};
@@ -34,7 +34,7 @@ insert_bt({}, E) -> {E, {}, {}, 1};
 insert_bt({W, LTree, RTree, H}, Ele) ->
   case (Ele < W) of
     % Ссылка установлена:
-    true -> case (isEmpty_bt(LTree)) of
+    true -> case (isempty_bt(LTree)) of
               % Левый слот свободен, поместите сюда новый лист
               % Если что-то висит с другой стороны, высота остается прежней
               true when (H > 1) -> {W, {Ele, {}, {}, 1}, RTree, H};
@@ -42,24 +42,24 @@ insert_bt({W, LTree, RTree, H}, Ele) ->
               true -> {W, {Ele, {}, {}, 1}, RTree, H + 1};
 
               % левый слот не свободен, переместите элемент ниже и дождитесь высоты следующего дерева
-              false -> {L_W, L_LT, L_RT, L_H} = insert_bt(LTree, Ele),
-                case (H > L_H) of
+              false -> {Lw, Llt, Lrt, Lh} = insert_bt(LTree, Ele),
+                case (H > Lh) of
                   % Если на другой стороне есть более длинная ветвь, H остается нетронутой
-                  true -> {W, {L_W, L_LT, L_RT, L_H}, RTree, H};
+                  true -> {W, {Lw, Llt, Lrt, Lh}, RTree, H};
                   % левое поддерево стало глубже, высоту нужно увеличить на 1
-                  false -> {W, {L_W, L_LT, L_RT, L_H}, RTree, H + 1}
+                  false -> {W, {Lw, Llt, Lrt, Lh}, RTree, H + 1}
                 end
             end;
 
     % Установлен справа:
-    false -> case (isEmpty_bt(RTree)) of
+    false -> case (isempty_bt(RTree)) of
                true when (H > 1) -> {W, LTree, {Ele, {}, {}, 1}, H};
                true -> {W, LTree, {Ele, {}, {}, 1}, H + 1};
 
-               false -> {R_W, R_LT, R_RT, R_H} = insert_bt(RTree, Ele),
-                 case (H > R_H) of
-                   true -> {W, LTree, {R_W, R_LT, R_RT, R_H}, H};
-                   false -> {W, LTree, {R_W, R_LT, R_RT, R_H}, H + 1}
+               false -> {Rw, Rlt, Rrt, Rh} = insert_bt(RTree, Ele),
+                 case (H > Rh) of
+                   true -> {W, LTree, {Rw, Rlt, Rrt, Rh}, H};
+                   false -> {W, LTree, {Rw, Rlt, Rrt, Rh}, H + 1}
                  end
              end
   end.
@@ -70,12 +70,12 @@ multiply_bt({}, {}) -> {};
 multiply_bt({}, Tree) -> Tree;
 multiply_bt(Tree, {}) -> Tree;
 multiply_bt({W1, _L1, _R1, _H1}, {W2, _L2, _R2, _H2}) -> lists:foldl(fun(X, Y) -> insert_bt(Y, X) end, {},
-  mulList({W1, _L1, _R1, _H1}, {W2, _L2, _R2, _H2})).
+  mul_list({W1, _L1, _R1, _H1}, {W2, _L2, _R2, _H2})).
 
-mulList({}, {}) -> [];
-mulList(_,{}) -> [];
-mulList({},_) -> [];
-mulList({W1, L1, R1, _H1}, {W2, L2, R2, _H2})-> [W1*W2] ++ mulList(L1,L2) ++ mulList(R1,R2).
+mul_list({}, {}) -> [];
+mul_list(_,{}) -> [];
+mul_list({},_) -> [];
+mul_list({W1, L1, R1, _H1}, {W2, L2, R2, _H2})-> [W1*W2] ++ mul_list(L1,L2) ++ mul_list(R1,R2).
 
 
 
@@ -103,20 +103,20 @@ filtration_tree_check(Fun, {W, L, R, _H}) ->
 
 
 % Сохранение всех значений дерева в список
-tree_to_List({}) -> [];
-tree_to_List({W, L, R, _H}) -> ([W] ++ tree_to_List(L) ++ tree_to_List(R)).
+tree_to_list({}) -> [];
+tree_to_list({W, L, R, _H}) -> ([W] ++ tree_to_list(L) ++ tree_to_list(R)).
 
 % сумма всех элементов дерева используя foldl
-sum_bt(BT) -> lists:foldl(fun(X, Y) -> X + Y end, 0, tree_to_List(BT)).
+sum_bt(BT) -> lists:foldl(fun(X, Y) -> X + Y end, 0, tree_to_list(BT)).
 
 % increase all elements in tree use map return new tree
 increase_bt(BT) ->
-  lists:foldl(fun(X, Y) -> insert_bt(Y, X) end, {}, lists:map(fun(X) -> X + 1 end, tree_to_List(BT))).
+  lists:foldl(fun(X, Y) -> insert_bt(Y, X) end, {}, lists:map(fun(X) -> X + 1 end, tree_to_list(BT))).
 
 
 % Проверяет наличие пустого дерева
-isEmpty_bt({}) -> true;
-isEmpty_bt(_) -> false.
+isempty_bt({}) -> true;
+isempty_bt(_) -> false.
 
 
 % значение и высота одинаковы, продолжить рекурсивно проверять левое и правое поддерево
@@ -133,19 +133,4 @@ my_max(_A, B) -> B.
 list_to_tree(List) -> lists:foldl(fun(X, Y) -> insert_bt(Y, X) end, {}, List).
 
 
-test_all() ->
-  X = btree:initBT(),
-  X1 = btree:insert_bt(X, 10),
-  X2 = btree:insert_bt(X1, 20),
-  X3 = btree:insert_bt(X2, 30),
-  X4 = btree:insert_bt(X3, 40),
-  X5 = btree:insert_bt(X4, 50),
-  X6 = btree:insert_bt(X5, 5),
-  io:format("~p~n", [multiply_bt(X5, X6)]),
-  io:format("~p~n", [multiply_bt(X6, X5)]),
-  io:format("~p~n", [add_tree(X1, X2)]),
-  io:format("~p~n", [add_tree(X2, X1)]),
-  io:format("X6 = ~p~n", [sum_bt(X6)]),
-  io:format("X6 = ~p~n", [increase_bt(X6)]),
-  io:format("X6 = ~p~n", [filtration_tree(fun(T) -> case T of 20 -> false; _ -> true end end, X6)]).
 
